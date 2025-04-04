da3fe622b278c42c7b5bb02147aa9fcfdd31a3a5
VanesaCorona <vanesaalondra012345@gmail.com>
2025-04-04 13:51:13 -0600

    cardPotentiia

--- app/welcome/CardSeccion3.tsx
+++ app/welcome/CardSeccion3.tsx
@@ -1,3 +1,66 @@
-export const card = () =>{
-    
+
+export const CardSeccion3 = () =>{
+    return(
+        <section className="flex flex-col items-center p-30">
+            <div className="text-center ">
+                <h1 className="text-[36px] font-bold uppercase">
+                Te entrenamos para desarrollar el súper poder del siglo XXI: <br />
+                ALTO rendimiEnto sostenible
+                </h1>
+
+                <p className="text-center text-gray-600 pt-5 pb-10 text-[20px]">
+                Rendir tu máxima potencia de forma consistente, cuidando tu salud y bienestar, es el super poder que desarrollamos en nuestros <br />
+                programas basados en el deporte profesional. Conoce nuestros programas de entrenamiento para optimizar salud, bienestar y <br />
+                rendimiento con protocoles prácticos , sostenibles y escalables.
+                </p>
+            </div>
+
+            <div className="flex flex-wrap justify-center gap-6 mt-8 ">
+                {/* Tarjeta 1 */}
+                <div className="bg-white rounded-2xl shadow-lg overflow-hidden max-w-xs">
+                    <div className="relative">
+                        <img className="w-full" src="https://s3-alpha-sig.figma.com/img/3db5/72ee/520d865459d444b7208b9aef1ce6f62c?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=gh1T01mCghS-qWWqf~RWcl0cH3o-HRd~LpMXZ6qpuydj4xB4P3dMB~EtupKL~kpDgTCrXKplc1V2g8FUAyajJKkP-FPcwLJtEbVZWbmGVSOk0Iye79~e5qJSe1mK~wV9U-OKtSdeajAVrgsjPPoElTUYklHirlQkbGHHrjANsD91g1horFShF7j-Bo4N~BdETsK265Sm0dYa80nynBpxQAfGha4HsaG4RCkGJAjMntadVepb8iTUaHgmakBlzWD0Wzt-cj0XhDzSrSxWNiA72qBmS0CJNfMYeVJ1~NOnvYtISOM6UmRgRVOUM0u17mX8D~Sz3T3JIKsAlILWxIws1A__" 
+                        alt="" />
+                    </div>
+                    <div className="p-4 ">
+                        <h2 className="text-lg font-bold">PROGRAMAS DE ENTRENAMIENTO INDIVIDUAL</h2>
+                        <p className="text-sm mt-2 text-gray-600">
+                            Lorem ipsum dolor sit amet consectetur. Dolor eu tortor elit mollis habitasse id. 
+                            Mi libero sit purus potenti. Mi libero sit purus potenti. Mi libero sit purus potenti.
+                        </p>
+                    </div>
+                </div>
+
+                {/* Tarjeta 2 */}
+                <div className="bg-white rounded-2xl shadow-lg overflow-hidden max-w-xs">
+                    <div className="relative">
+                        <img className="w-full" src="https://s3-alpha-sig.figma.com/img/3db5/72ee/520d865459d444b7208b9aef1ce6f62c?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=gh1T01mCghS-qWWqf~RWcl0cH3o-HRd~LpMXZ6qpuydj4xB4P3dMB~EtupKL~kpDgTCrXKplc1V2g8FUAyajJKkP-FPcwLJtEbVZWbmGVSOk0Iye79~e5qJSe1mK~wV9U-OKtSdeajAVrgsjPPoElTUYklHirlQkbGHHrjANsD91g1horFShF7j-Bo4N~BdETsK265Sm0dYa80nynBpxQAfGha4HsaG4RCkGJAjMntadVepb8iTUaHgmakBlzWD0Wzt-cj0XhDzSrSxWNiA72qBmS0CJNfMYeVJ1~NOnvYtISOM6UmRgRVOUM0u17mX8D~Sz3T3JIKsAlILWxIws1A__" 
+                        alt="" />
+                    </div>
+                    <div className="p-4 ">
+                        <h2 className="text-lg font-bold">PROGRAMAS DE ENTRENAMIENTO PARA EMPRESAS</h2>
+                        <p className="text-sm mt-2 text-gray-600">
+                            Lorem ipsum dolor sit amet consectetur. Dolor eu tortor elit mollis habitasse id. 
+                            Mi libero sit purus potenti. Mi libero sit purus potenti. Mi libero sit purus potenti.
+                        </p>
+                    </div>
+                </div>
+
+                {/* Tarjeta 3 */}
+                <div className="bg-white rounded-2xl shadow-lg overflow-hidden max-w-xs">
+                    <div className="relative">
+                        <img className="w-full" src="https://s3-alpha-sig.figma.com/img/3db5/72ee/520d865459d444b7208b9aef1ce6f62c?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=gh1T01mCghS-qWWqf~RWcl0cH3o-HRd~LpMXZ6qpuydj4xB4P3dMB~EtupKL~kpDgTCrXKplc1V2g8FUAyajJKkP-FPcwLJtEbVZWbmGVSOk0Iye79~e5qJSe1mK~wV9U-OKtSdeajAVrgsjPPoElTUYklHirlQkbGHHrjANsD91g1horFShF7j-Bo4N~BdETsK265Sm0dYa80nynBpxQAfGha4HsaG4RCkGJAjMntadVepb8iTUaHgmakBlzWD0Wzt-cj0XhDzSrSxWNiA72qBmS0CJNfMYeVJ1~NOnvYtISOM6UmRgRVOUM0u17mX8D~Sz3T3JIKsAlILWxIws1A__" 
+                        alt="" />
+                    </div>
+                    <div className="p-4 ">
+                        <h2 className="text-lg font-bold">PROGRAMAS DE ENTRENAMIENTO PARA INSTITUCIONES DEPORTIVAS</h2>
+                        <p className="text-sm mt-2 text-gray-600">
+                            Lorem ipsum dolor sit amet consectetur. Dolor eu tortor elit mollis habitasse id. 
+                            Mi libero sit purus potenti. Mi libero sit purus potenti. Mi libero sit purus potenti.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -3,6 +3,7 @@ import { Hero } from "./Hero";
 import { Secccion1 } from "./Seccion1";
 import { Carrusell } from "./Carrusell";
 import { Seccion2 } from "./Seccion2";
+import { CardSeccion3 } from "./CardSeccion3";
 
 export function Welcome() {
   return (
@@ -12,6 +13,7 @@ export function Welcome() {
       <Secccion1 />
       <Carrusell/>
       <Seccion2/>
+      <CardSeccion3/>
     </main>
   );
 }
